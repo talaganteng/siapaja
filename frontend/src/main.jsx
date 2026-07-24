87601556b321a45c4cf5dc486659f18936b0ab4a
Siapaja Deployment <deploy@siapaja.com>
2026-07-24 20:48:34 +0700

    Add localtunnel bypass header

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Global Fetch Interceptor to bypass LocalTunnel anti-phishing warning
+const originalFetch = window.fetch;
+window.fetch = async (...args) => {
+  let [resource, config] = args;
+  if (!config) config = {};
+  if (!config.headers) config.headers = {};
+  
+  if (config.headers instanceof Headers) {
+    config.headers.append('Bypass-Tunnel-Reminder', 'true');
+  } else {
+    config.headers['Bypass-Tunnel-Reminder'] = 'true';
+  }
+  
+  return originalFetch(resource, config);
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
